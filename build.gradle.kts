cf225ccd4ad5ec2db38fa34b6822bf6e6a794d55
Sascha Bieberstein <sbi@scireum.de>
2025-04-30 22:23:06 +0200

    Ensure future versions of IntelliJ are supported

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.scireum"
-version = "1.1"
+version = "1.2"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,6 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("243.*")
     }
 
     signPlugin {
